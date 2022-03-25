e57e42dd172047f0f3e0d820ec3235605c53211b
Aparna Panda <aparnapanda55@gmail.com>
2022-03-25 10:00:19 +0530

    constrained width for web

--- lib/main.dart
+++ lib/main.dart
@@ -133,86 +133,93 @@ class MyHomePage extends StatelessWidget {
 
           final groups = groupTodosByDate(todos);
 
-          return ListView(
-            children: groups.map((group) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Text(
-                        group.heading,
-                        style: const TextStyle(fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                    Card(
-                      elevation: 3,
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 15,
+          return Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 600),
+              child: ListView(
+                children: groups.map((group) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Text(
+                            group.heading,
+                            style: const TextStyle(fontWeight: FontWeight.w500),
+                          ),
                         ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            left: BorderSide(
-                              color: group.color,
-                              width: 6,
+                        Card(
+                          elevation: 3,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 15,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                left: BorderSide(
+                                  color: group.color,
+                                  width: 6,
+                                ),
+                              ),
+                            ),
+                            child: Opacity(
+                              opacity: group.isDone ? 0.2 : 1,
+                              child: Column(
+                                children: group.todos
+                                    .map(
+                                      (todo) => Dismissible(
+                                        key: ValueKey(todo.id!),
+                                        onDismissed: (direction) {
+                                          _collection.doc(todo.id).delete();
+                                        },
+                                        child: ListTile(
+                                          title: Text(
+                                            todo.text,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                            ),
+                                          ),
+                                          trailing: Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              Text(
+                                                todo.time,
+                                                style: const TextStyle(
+                                                  fontWeight: FontWeight.w500,
+                                                ),
+                                              ),
+                                              Checkbox(
+                                                hoverColor: Colors.teal,
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                                value: todo.isDone,
+                                                onChanged: (value) {
+                                                  _collection
+                                                      .doc(todo.id)
+                                                      .update(
+                                                          {'isDone': value});
+                                                },
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
                             ),
                           ),
                         ),
-                        child: Opacity(
-                          opacity: group.isDone ? 0.2 : 1,
-                          child: Column(
-                            children: group.todos
-                                .map(
-                                  (todo) => Dismissible(
-                                    key: ValueKey(todo.id!),
-                                    onDismissed: (direction) {
-                                      _collection.doc(todo.id).delete();
-                                    },
-                                    child: ListTile(
-                                      title: Text(
-                                        todo.text,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                      trailing: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Text(
-                                            todo.time,
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          Checkbox(
-                                            hoverColor: Colors.teal,
-                                            shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(10)),
-                                            value: todo.isDone,
-                                            onChanged: (value) {
-                                              _collection
-                                                  .doc(todo.id)
-                                                  .update({'isDone': value});
-                                            },
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                )
-                                .toList(),
-                          ),
-                        ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
-              );
-            }).toList(),
+                  );
+                }).toList(),
+              ),
+            ),
           );
         },
       ),

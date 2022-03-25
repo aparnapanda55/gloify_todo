1ddfab01b51177bc43b702a7dbc3418a4f8d40f6
Aparna Panda <aparnapanda55@gmail.com>
2022-03-25 12:27:33 +0530

    separated todogroupcard to its file

--- lib/widgets/todo_group_card.dart
+++ lib/widgets/todo_group_card.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../models.dart';
 
 class TodoGroupCard extends StatelessWidget {
   const TodoGroupCard({
